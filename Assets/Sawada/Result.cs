22c01780bd67d03693f0bc9d5d87964eded74f13
SR21C <rysawad@gmail.com>
2021-11-24 14:53:49 +0900

    リザルトスクリプト

---
+++ Assets/Sawada/Result.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Result : MonoBehaviour
+{
+    [SerializeField] Text resultText = default;
+    [SerializeField] Text newRecord;
+    ScoreManager scoreManager = default;
+    static int beforeResult;
+    int maxScore = 999999999;
+    int moneyResult;
+    float scoreChangeInterval = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = GetComponent<ScoreManager>();
+        moneyResult = scoreManager.moneyScore;
+        int tempScore = moneyResult; 
+        moneyResult = Mathf.Min(moneyResult + moneyResult, maxScore);
+        if (tempScore != maxScore)
+        {
+            DOTween.To(() => tempScore,
+                x => tempScore = x,
+                moneyResult,
+                scoreChangeInterval)
+                .OnUpdate(() => resultText.text = tempScore.ToString())
+                .OnComplete(() => resultText.text = moneyResult.ToString());
+        }
+        if (moneyResult > beforeResult)
+        {
+            newRecord.enabled = true;
+            beforeResult = moneyResult;
+        }
+        else if (moneyResult <= beforeResult) newRecord.enabled = false;
+    }
+
+}
--- Assets/Sawada/ScoreManager.cs
+++ Assets/Sawada/ScoreManager.cs
@@ -6,28 +6,28 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] Text scoerText;
-    [SerializeField] int moneyScoer = 0;
-    //GamaManager gamaManager = default;
+    public int moneyScore = 0;
+    GameManager gameManager = default;
     bool live;
     
     void Start()
     {
         scoerText.color = new Color(1, 1, 1, 1);
-        //gamaManager = GetComponent<GamaManager>();
+        gameManager = GetComponent<GameManager>();
     }
 
     void Update()
     {
-        scoerText.text = "MONEY " + moneyScoer.ToString("D10");
-        if (moneyScoer >= 10 && moneyScoer < 100) scoerText.color = new Color(0, 1, 0, 1);
-        else if (moneyScoer >= 100 && moneyScoer < 1000) scoerText.color = new Color(0, 0, 1, 1);
-        else if (moneyScoer >= 1000 && moneyScoer < 10000) scoerText.color = new Color(1, 0, 1, 1);
-        else if (moneyScoer >= 10000 && moneyScoer < 100000) scoerText.color = new Color(1, 0.92f, 0.016f, 1);
-        else if (moneyScoer >= 100000&& moneyScoer < 1000000) scoerText.color = new Color(1, 0, 0, 1);
+        scoerText.text = "MONEY " + moneyScore.ToString();
+        if (moneyScore >= 10 && moneyScore < 100) scoerText.color = new Color(0, 1, 0, 1);
+        else if (moneyScore >= 100 && moneyScore < 1000) scoerText.color = new Color(0, 0, 1, 1);
+        else if (moneyScore >= 1000 && moneyScore < 10000) scoerText.color = new Color(1, 0, 1, 1);
+        else if (moneyScore >= 10000 && moneyScore < 100000) scoerText.color = new Color(1, 0.92f, 0.016f, 1);
+        else if (moneyScore >= 100000&& moneyScore < 1000000) scoerText.color = new Color(1, 0, 0, 1);
 
         if(!live)
         {
-
+            gameManager.ChangeResultScene();
         }
     }
 }

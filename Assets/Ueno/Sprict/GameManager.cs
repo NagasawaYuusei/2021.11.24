3305b1449d8209d9b43646f6efb5a45a26addd2f
uen0_ <sarasa4869@zoho.com>
2021-11-24 12:33:17 +0900

    Uenoファイル追加

---
+++ Assets/Ueno/Sprict/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    //GameStart_"MainScene"にシーン遷移
+    public void ChangeGameStart() 
+    {
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("ゲームスタート");
+    }
+    //TitleScene_"TitleScene"にシーン遷移
+    public void ChangeTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+        Debug.Log("タイトルシーンに戻る");
+    }
+}
